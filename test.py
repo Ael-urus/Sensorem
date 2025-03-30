c3bdcf7db3ea15061245e351964f3ad1ad149d78
Aelurus <110226698+Ael-urus@users.noreply.github.com>
2025-03-30 19:04:18 +0200

    Delete test.py
    no needs

--- test.py
+++
@@ -1,9 +0,0 @@
-y = [1, 2, 3]
-y2 = [4, 5, 6]
-
-# Assurez-vous que les deux listes ont la même longueur
-if len(y) == len(y2):
-    for i in range(len(y)):
-        print(f"{y[i]}\t{y2[i]}")
-else:
-    print("Les listes n'ont pas la même longueur.")
