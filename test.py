8b9c69d77b28bfd40fb08093a6229df25c0ca491
Aelurus <110226698+Ael-urus@users.noreply.github.com>
2025-03-30 19:41:53 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import FonctionCSV as fc
-
-# Utilisez la fonction read_col_CSV avec les paramètres nécessaires
-fichier = "DebudFindeFichier.csv"
-sep = ";"
-n = 2
-result = fc.read_col_CSV(fichier, sep, n)
-
-print(result)
